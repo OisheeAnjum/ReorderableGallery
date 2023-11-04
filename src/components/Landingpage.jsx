8113995ee1b0794b291d7d83da46ef5ce23d757e
oishee <oishee42@gmail.com>
2023-11-05 01:39:48 +0600

    used new aproach for swapping

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,10 +1,7 @@
-/* eslint-disable react/no-array-index-key */
-/* eslint-disable no-shadow */
 /* eslint-disable react/jsx-props-no-spreading */
 import React, { useState } from 'react';
-import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 import { Col, Container, Row } from 'react-bootstrap';
-
+import { GridContextProvider, GridDropZone, GridItem, swap } from 'react-grid-dnd';
 import { Button } from 'rsuite';
 import image1 from '../assets/images/T1It0qMA.png';
 import image2 from '../assets/images/U6OVh27Q.png';
@@ -41,6 +38,14 @@ export default function Landingpage() {
             id: 4,
             src: image5,
         },
+        {
+            id: 5,
+            src: image4,
+        },
+        {
+            id: 6,
+            src: image5,
+        },
         // Add more images as needed
     ];
 
@@ -60,32 +65,26 @@ export default function Landingpage() {
         });
     };
 
-    const handleDragEnd = (result) => {
-        if (!result.destination) {
-            return;
-        }
+    // const handleDragEnd = (result) => {
+    //     if (!result.destination) {
+    //         return;
+    //     }
 
-        const reorderedImages = [...imageList];
-        const [movedImage] = reorderedImages.splice(result.source.index, 1);
-        reorderedImages.splice(result.destination.index, 0, movedImage);
+    //     const reorderedImages = [...imageList];
+    //     const [movedImage] = reorderedImages.splice(result.source.index, 1);
+    //     reorderedImages.splice(result.destination.index, 0, movedImage);
 
-        setImageList(reorderedImages);
-    };
+    //     setImageList(reorderedImages);
+    // };
     const handleImageRemove = (selectedImageIds) => {
         const updatedImageList = imageList.filter((image) => !selectedImageIds.includes(image.id));
         setImageList(updatedImageList);
     };
-    // const grid = 8;
-    const getListStyle = (isDraggingOver) => ({
-        background: isDraggingOver ? 'lightblue' : 'lightgrey',
-        overflow: 'scroll',
-    });
 
-    // const getItemStyle = () => ({
-    //     height: '22rem',
-    //     width: '22rem',
-    // });
-
+    const onChange = (sourceId: string, sourceIndex: number, targetIndex: number) => {
+        const nextState = swap(imageList, sourceIndex, targetIndex);
+        setImageList(nextState);
+    };
     return (
         <Container className="mt-4">
             <Imageupload onImageUpload={handleImageUpload} onImageRemove={handleImageRemove} />
@@ -99,63 +98,43 @@ export default function Landingpage() {
                     />
                 </Col>
                 <Col md={9}>
-                    <DragDropContext onDragEnd={handleDragEnd}>
-                        <Droppable droppableId="image-gallery" direction="horizontal">
-                            {(provided, snapshot) => (
-                                <div
-                                    ref={provided.innerRef}
-                                    {...provided.droppableProps}
-                                    style={getListStyle(snapshot.isDraggingOver)}
-                                >
-                                    <Row>
-                                        {imageList.map((item, index) => (
-                                            <Draggable
-                                                draggableId={`draggable-${item.id}`}
-                                                key={`draggable-${item.id}`}
-                                                index={index}
-                                            >
-                                                {(provided) => (
-                                                    <Col
-                                                        md={3}
-                                                        {...provided.draggableProps}
-                                                        {...provided.dragHandleProps}
-                                                    >
-                                                        <div className="position-relative ">
-                                                            <img
-                                                                ref={provided.innerRef}
-                                                                className={`img-fluid  rounded rounded-1 m-1 ${
-                                                                    index === 0
-                                                                        ? 'yellow-border'
-                                                                        : 'border'
-                                                                }`}
-                                                                src={item.src}
-                                                                alt={` ${index}`}
-                                                            />
-                                                            <input
-                                                                className="position-absolute"
-                                                                type="checkbox"
-                                                                checked={selectedImages.includes(
-                                                                    item.id
-                                                                )}
-                                                                onChange={() =>
-                                                                    handleImageSelect(item.id)
-                                                                }
-                                                                style={{
-                                                                    zIndex: '10',
-                                                                    top: '8px',
-                                                                    right: '2px',
-                                                                }}
-                                                            />
-                                                        </div>
-                                                    </Col>
-                                                )}
-                                            </Draggable>
-                                        ))}
-                                    </Row>
-                                </div>
-                            )}
-                        </Droppable>
-                    </DragDropContext>
+                    <GridContextProvider onChange={onChange}>
+                        <GridDropZone
+                            id="items"
+                            boxesPerRow={4}
+                            rowHeight={150}
+                            style={{ height: '430px' }}
+                        >
+                            {imageList.map((item, index) => (
+                                <GridItem key={item.id}>
+                                    <div
+                                        className={`position-relative d-flex justify-content-center align-items-center rounded rounded-1 m-1 ${
+                                            index === 0 ? 'yellow-border' : 'border'
+                                        }`}
+                                        style={{ height: '8rem' }}
+                                    >
+                                        <img
+                                            className="img-fluid"
+                                            src={item.src}
+                                            alt={` ${index}`}
+                                            style={{ maxHeight: '8rem' }}
+                                        />
+                                        <input
+                                            className="position-absolute"
+                                            type="checkbox"
+                                            checked={selectedImages.includes(item.id)}
+                                            onChange={() => handleImageSelect(item.id)}
+                                            style={{
+                                                zIndex: '10',
+                                                top: '8px',
+                                                right: '2px',
+                                            }}
+                                        />
+                                    </div>
+                                </GridItem>
+                            ))}
+                        </GridDropZone>
+                    </GridContextProvider>
                 </Col>
             </Row>
         </Container>
